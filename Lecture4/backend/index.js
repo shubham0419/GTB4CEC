56b768ace5e76a61f9ddbf1d06830669421719b1
shubham0419 <shubhamsc704707@gmail.com>
2025-04-08 10:51:42 +0530

    lecture 4 backend

--- Lecture4/backend/index.js
+++ Lecture4/backend/index.js
@@ -7,41 +7,69 @@ app.use(express.urlencoded({ extended: true }));
 
 const TODOS = [
   {
-    id:1,
-    title:"React",
-    description:"Learning React"
-  }
-]
+    id: 1,
+    title: "React",
+    description: "Learning React",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.get("/alltodos",async(req,res)=>{
+app.get("/alltodos", async (req, res) => {
   try {
-    res.status(200).json({TODOS});
+    res.status(200).json({ TODOS });
   } catch (error) {
-    res.status(402).json({message:error.message})
+    res.status(402).json({ message: error.message });
   }
-})
+});
 
-app.post("/create",async(req,res)=>{
+app.post("/create", async (req, res) => {
   try {
-    const {title,description} = req.body;
-    if(!title || !description){
-      res.status(401).json({message:"title and description are required"})
+    const { title, description } = req.body;
+    if (!title || !description) {
+      res.status(401).json({ message: "title and description are required" });
     }
     TODOS.push({
-      id:Math.random(),
+      id: Math.random(),
       title,
-      description
+      description,
     });
-    res.status(200).json({TODOS});
+    res.status(200).json({ TODOS });
   } catch (error) {
-    res.status(402).json({message:error.message})
+    res.status(402).json({ message: error.message });
+  }
+});
+
+app.put("/update/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    const todo = TODOS.find((td) => {
+      td.id == id;
+    });
+    const ind = TODOS.indexOf(todo);
+    TODOS[ind] = {id,title,description};
+    res.status(200).json({ TODOS });
+  } catch (error) {
+    res.status(402).json({ message: error.message });
+  }
+});
+
+app.delete("/delete/:id",(req,res)=>{
+  try {
+    const { id } = req.params;
+    const todo = TODOS.find((td) => {
+      td.id == id;
+    });
+    const ind = TODOS.indexOf(todo);
+    TODOS.splice(ind,1);
+    res.status(200).json({ TODOS });
+  } catch (error) {
+    res.status(402).json({ message: error.message });
   }
 })
-
-app.put("/update/:id")
 
 app.listen(PORT, () => console.log("Server running on port " + PORT));
