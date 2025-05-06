e81116fa8041d02cef1f9cd38575a825aea6f96a
shubham0419 <shubhamsc704707@gmail.com>
2025-05-06 10:44:43 +0530

    product controller

--- lecture5/backend/routes/product.route.js
+++ lecture5/backend/routes/product.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createProduct } = require("../controller/product.controller");
+const { createProduct, getAllProducts, updateProduct, deleteProduct, getProductsByCategory } = require("../controller/product.controller");
 const { isAdmin } = require("../middleware");
 const router = express.Router();
 
-router.post("/create",isAdmin,createProduct)
+router.post("/create",isAdmin,createProduct);
+router.put("/update/:id",isAdmin,updateProduct);
+router.delete("/delete/:id",isAdmin,deleteProduct);
+router.get("/",getAllProducts);
+router.get("/category/:id",getProductsByCategory);
 
-module.exports = router
+module.exports = router;
