99ee6dcad1647e1da223bc2b2994f313d91714b8
shubham0419 <shubhamsc704707@gmail.com>
2025-05-06 10:40:04 +0530

    product controller

--- lecture5/backend/controller/product.controller.js
+++ lecture5/backend/controller/product.controller.js
@@ -1,6 +1,5 @@
 const Product = require("../model/product.model")
 
-
 const createProduct = async (req,res)=>{
   try {
     const product = await Product.create(req.body);
@@ -10,4 +9,64 @@ const createProduct = async (req,res)=>{
   }
 }
 
-module.exports = {createProduct};
+const updateProduct = async(req,res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.updateOne({_id:id},req.body);
+    res.status(200).json({message:"Product updated successsfully",product})
+  } catch (error) {
+    res.status(402).json({message:error.message});
+  }
+}
+
+const deleteProduct = async(req,res)=>{
+  try {
+    const {id} = req.params;
+    const product = await Product.deleteOne({_id:id});
+    res.status(200).json({message:"Product deleted successsfully",product})
+  } catch (error) {
+    res.status(402).json({message:error.message});
+  }
+}
+
+const getAllProducts = async(req,res)=>{
+  try {
+    const {name,sortby,sortOrder} = req.query;
+
+    const pipline = []
+    if(name){
+      const nameQuery = {
+        $match: {
+          // Your search criteria here
+          // Example: Search for documents where 'name' contains 'abc'
+          name: { $regex: name, $options: 'i' },
+        },
+      }
+      pipline.push(nameQuery);
+    }
+    if(sortby){
+      const sortquery = {
+        $sort:{
+          sortby: sortOrder=="asc"? 1 : -1, // sortby is field(price, name ) and 1 stands for ascending order ,-1 stand for descending order
+        }  
+      }
+      pipline.push(sortquery);
+    }
+    const products = await Product.aggregate(pipline).exec();
+    res.status(200).json({message:"product found successfully",products})
+  } catch (error) {
+    res.status(402).json({message:error.message});
+  }
+}
+
+const getProductsByCategory = async(req,res)=>{
+  try {
+    const {id} = req.params;
+    const products = await Product.find({category:id});
+    res.status(200).json({message:"Products fetched successfully",products})
+  } catch (error) {
+    res.status(402).json({message:error.message});
+  }
+}
+
+module.exports = {createProduct,updateProduct,deleteProduct,getAllProducts,getProductsByCategory};
