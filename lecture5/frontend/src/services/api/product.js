5c13453d63040928a6df631686d239aa4bd4af42
shubham0419 <shubhamsc704707@gmail.com>
2025-05-07 11:28:42 +0530

    redux

--- lecture5/frontend/src/services/api/product.js
+++ lecture5/frontend/src/services/api/product.js
@@ -3,8 +3,31 @@ import axios from "axios"
 const BaseUrl = "http://localhost:4000"
 
 
-export const getAllProducts = async () => {
-  
+export const getAllProducts = async (name,sortby,sortOrder,color,gender,size,wearType,productType) => {
+  const params = {};
+  if(name){
+    params.name = name;
+  }
+  if(sortby){
+    params.sortby = sortby;
+    params.sortOrder = sortOrder;
+  }
+  if(color){
+    params.color = color
+  }
+  if(gender){
+    params.gender = gender;
+  }
+  if(size){
+    params.size = size;
+  }
+  if(wearType){
+    params.wearType = wearType;
+  }
+  if(productType){
+    params.productType = productType;
+  }
+
 }
 
 export const getProductsByCategory = async (category) => {
